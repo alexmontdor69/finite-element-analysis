e338f436a0cc517a3cdb0f6dc7b82b7f7b472e5d
alexmontdor69 <Alexandre LANDREAU>
2020-12-17 18:55:24 +0000

    Initiating file parsing

--- sofea.cpp
+++ sofea.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
 #include <math.h>
 
 #include "tools.h"
@@ -14,10 +15,21 @@
 
 using namespace std;
 
-int main()
+//functions declaration
+void display_greetings(void);
+
+int main(int argc, char **argv)
 {
+	display_greetings();
+
+	char *filename = "data.txt";
+	// Take the first argument to read the file
+	if (argc > 1)
+		filename = argv[1];
+	std::cout << "\n Parsing model file " << filename << " ...";
+
 	// Objects Creation
-	FILE *file;
+	std::ifstream model_file(filename);
 	Node *DNodes;
 
 	// Object the material
@@ -30,7 +42,8 @@ int main()
 	Beam3 *DBeam3[3000];
 	Plane42 *DPlane42[3000];
 
-	char filename[] = "c:\\sofea\\data.txt", Command[100], *ptr;
+	char Command[100], *ptr;
+
 	long NbElems = 0, NbNodes = 0, Number, NbObjectCreated = 0;
 	int inc, countlev1, *indx, MaxNode = 2;
 	long count[256] = {0}, num; // to know how objects created per kind of element
@@ -40,9 +53,19 @@ int main()
 	// Function to read the file
 	// Function to build the matrix
 	// Function to solve the model
+	// Setting the model filename
 
-	printf("\n Reading file ...");
-	if ((file = fopen(filename, "r")) != NULL)
+	if (model_file.is_open())
+	{
+		std::cout << "\ncan read " << filename;
+	}
+	else
+	{
+		std::cout << "\nCan't read " << filename;
+	}
+
+	/*
+	if ((model_file = fopen(filename, "r")) != NULL)
 	{
 		while (!::EndOfFile)
 		{
@@ -96,12 +119,9 @@ int main()
 					if (countlev1 == 1 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
 					{
 						DLink1[count[countlev1]] = new Link1(file, Number, DNodes); // Replace by a constructor
-						count[countlev1]++;
-						NbObjectCreated++;
-					}
+					cout << "\n Reading file " << filename << " ...";
 
-					// Plane2 !
-					if (countlev1 == 2 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
+	model_file.open(model_file, iosif (countlev1 == 2 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
 					{
 						DPlane2[count[countlev1]] = new Plane2(file, Number, DNodes);
 						count[countlev1]++;
@@ -125,7 +145,9 @@ int main()
 					NextWord(file, &Command[0]);
 				}
 			}
+cout << "\n Reading file " << filename << " ...";
 
+	model_file.open(model_file, ios
 			// Material data reading process
 			if (!strcmp("MAT", Command) && !::EndOfFile)
 			{
@@ -197,50 +219,52 @@ int main()
 				}
 			}
 		}
-
 		printf(" completed!\n");
-		printf("Global Matrix building...");
+		fclose(file);
+	}
+*/
+	std::cout << "Global Matrix building...";
 
-		// Global matrix creation ! square matrix for the moment
-		double **Global = new double *[MaxNode];
-		for (inc = 0; inc < MaxNode; inc++)
-			Global[inc] = new double[MaxNode];
+	// Global matrix creation ! square matrix for the moment
+	double **Global = new double *[MaxNode];
+	for (inc = 0; inc < MaxNode; inc++)
+		Global[inc] = new double[MaxNode];
 
-		// Initialization of Global Matrix
-		for (inc = 0; inc < MaxNode; inc++)
-			for (int inc2 = 0; inc2 < MaxNode; inc2++)
-				Global[inc][inc2] = 0;
+	// Initialization of Global Matrix
+	for (inc = 0; inc < MaxNode; inc++)
+		for (int inc2 = 0; inc2 < MaxNode; inc2++)
+			Global[inc][inc2] = 0;
 
-		// Assemble Matrix
-		for (inc = 0; inc < count[1]; inc++)
-		{
-			DLink1[inc]->CalculateLength(DNodes, DMat);
-			DLink1[inc]->AssembleMatrix(Global, MaxNode, DNodes);
-		}
-		for (inc = 0; inc < count[2]; inc++)
-		{
-			DPlane2[inc]->CalculateLength(DNodes, DMat);
-			DPlane2[inc]->AssembleMatrix(Global, MaxNode, DNodes);
-		}
+	// Assemble Matrix
+	for (inc = 0; inc < count[1]; inc++)
+	{
+		DLink1[inc]->CalculateLength(DNodes, DMat);
+		DLink1[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+	}
+	for (inc = 0; inc < count[2]; inc++)
+	{
+		DPlane2[inc]->CalculateLength(DNodes, DMat);
+		DPlane2[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+	}
 
-		for (inc = 0; inc < count[3]; inc++)
-		{
-			DBeam3[inc]->CalculateLength(DNodes, DMat);
-			DBeam3[inc]->AssembleMatrix(Global, MaxNode, DNodes);
-		}
-		for (inc = 0; inc < count[42]; inc++)
-		{
-			DPlane42[inc]->CalculateMatrix(DNodes, DMat);
-			DPlane42[inc]->AssembleMatrix(Global, MaxNode, DNodes);
-		}
+	for (inc = 0; inc < count[3]; inc++)
+	{
+		DBeam3[inc]->CalculateLength(DNodes, DMat);
+		DBeam3[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+	}
+	for (inc = 0; inc < count[42]; inc++)
+	{
+		DPlane42[inc]->CalculateMatrix(DNodes, DMat);
+		DPlane42[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+	}
 
-		// It is a "diagonal matrix" so to implement Ks only on the diagonal so far
-		// implementation of Ks function of the Maximum degree of freedom to locate the Node
-		for (inc = 0; inc < MaxNode; inc++)
-			if (indx[inc] == 0)
-				Global[inc][inc] = 1e200;
+	// It is a "diagonal matrix" so to implement Ks only on the diagonal so far
+	// implementation of Ks function of the Maximum degree of freedom to locate the Node
+	/* for (inc = 0; inc < MaxNode; inc++)
+		if (indx[inc] == 0)
+			Global[inc][inc] = 1e200; */
 
-		/*
+	/*
 cout<<"\n"<<"Global matrix"<<"\n";
 	// Globlal Matrix display
 for (inc=0; inc<MaxNode;inc++)
@@ -251,43 +275,50 @@ for (inc=0; inc<MaxNode;inc++)
 	cout<<"\n";
 }
 */
-		printf("completed!\n");
-		printf("Solving by LU decomposition...");
-		// Solving part______________________________________________________________________________
-		//display the Result the backsubstition put all the result in the Forces vector..
-		//(unfortunatly deleting them)
-		// First step use the tool header and the Lower Upper Decomposition
-		ludcmp(Global, MaxNode, indx, Forces);
+	printf("completed!\n");
+	printf("Solving by LU decomposition...");
+	// Solving part______________________________________________________________________________
+	//display the Result the backsubstition put all the result in the Forces vector..
+	//(unfortunatly deleting them)
+	// First step use the tool header and the Lower Upper Decomposition
 
-		// Then let's do a back substitution to get the result
-		lubksb(Global, MaxNode, indx, Forces);
+	//ludcmp(Global, MaxNode, indx, Forces);
 
-		printf("completed!\nDisplacements sorting process...");
-		max = 0;
-		for (inc = 0; inc < NbNodes; inc++)
+	// Then let's do a back substitution to get the result
+	//lubksb(Global, MaxNode, indx, Forces);
+
+	printf("completed!\nDisplacements sorting process...");
+	max = 0;
+	for (inc = 0; inc < NbNodes; inc++)
+	{
+		//	cout<<"\n Node "
+		//		<< inc+1;
+		//	for (int inc2=0;inc2 < DNodes[inc].DOF;inc2++)
+		//	{
+		//		cout<<"\n"
+		//			<<Forces[DNodes[inc].CumSum+ inc2];
+		//	}
+		if ((sqrt(pow(Forces[DNodes[inc].CumSum], 2) + pow(Forces[DNodes[inc].CumSum + 1], 2))) > max)
 		{
-			//	cout<<"\n Node "
-			//		<< inc+1;
-			//	for (int inc2=0;inc2 < DNodes[inc].DOF;inc2++)
-			//	{
-			//		cout<<"\n"
-			//			<<Forces[DNodes[inc].CumSum+ inc2];
-			//	}
-			if ((sqrt(pow(Forces[DNodes[inc].CumSum], 2) + pow(Forces[DNodes[inc].CumSum + 1], 2))) > max)
-			{
-				max = sqrt(pow(Forces[DNodes[inc].CumSum], 2) + pow(Forces[DNodes[inc].CumSum + 1], 2));
-				num = inc + 1;
-			}
+			max = sqrt(pow(Forces[DNodes[inc].CumSum], 2) + pow(Forces[DNodes[inc].CumSum + 1], 2));
+			num = inc + 1;
 		}
-		printf("completed!\n");
-		cout << "\nThe Greater displacement is : " << max
-			 << " and the Node number of the greater element is : " << num;
-
-		cout << "\nux = " << Forces[DNodes[num - 1].CumSum]
-			 << " and uy = " << Forces[DNodes[num - 1].CumSum + 1];
-
-		cout << "\nThis problem includes " << NbNodes << " nodes and " << NbElems << " elements.\n";
-		fclose(file);
 	}
+	printf("completed!\n");
+	cout << "\nThe Greater displacement is : " << max
+		 << " and the Node number of the greater element is : " << num;
+
+	cout << "\nux = " << Forces[DNodes[num - 1].CumSum]
+		 << " and uy = " << Forces[DNodes[num - 1].CumSum + 1];
+
+	cout << "\nThis problem includes " << NbNodes << " nodes and " << NbElems << " elements.\n";
+
 	return 0;
+}
+
+void display_greetings(void)
+{
+	// Greetings
+	std::cout << "\nWELCOME TO SOFEA";
+	std::cout << "\n----------------\n\n";
 }
