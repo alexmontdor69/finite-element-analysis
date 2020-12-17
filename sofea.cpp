7428d3edab2faad7a42e8aad958ba0e818a8942e
alexmontdor69 <Alexandre LANDREAU>
2020-12-17 20:31:42 +0000

    Establish a data structure and start parsing

--- sofea.cpp
+++ sofea.cpp
@@ -1,9 +1,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 #include <iostream>
 #include <fstream>
-#include <math.h>
+#include <algorithm>
 
 #include "tools.h"
 
@@ -26,17 +28,17 @@ int main(int argc, char **argv)
 	// Take the first argument to read the file
 	if (argc > 1)
 		filename = argv[1];
-	std::cout << "\n Parsing model file " << filename << " ...";
+	std::cout << "\n Model file : " << filename << " ...";
 
-	// Objects Creation
+	// Opening model file for Parsing
 	std::ifstream model_file(filename);
-	Node *DNodes;
 
-	// Object the material
+	// Init Side information
+	Node *DNodes;
 	Material *DMat;
 	DMat = new Material[2];
 
-	// Object for the elements
+	// Init elements
 	Link1 *DLink1[300];
 	Plane2 *DPlane2[3000];
 	Beam3 *DBeam3[3000];
@@ -45,6 +47,7 @@ int main(int argc, char **argv)
 	char Command[100], *ptr;
 
 	long NbElems = 0, NbNodes = 0, Number, NbObjectCreated = 0;
+
 	int inc, countlev1, *indx, MaxNode = 2;
 	long count[256] = {0}, num; // to know how objects created per kind of element
 	double *Forces, max;
@@ -57,210 +60,272 @@ int main(int argc, char **argv)
 
 	if (model_file.is_open())
 	{
-		std::cout << "\ncan read " << filename;
+		std::cout << "\nParsing File\n"
+				  << filename;
+		//parsing_file(model_file);
+		std::string line;
+		while (getline(model_file, line))
+		{
+			line.erase(std::remove_if(line.begin(), line.end(), [](char c) {
+						   return std::isspace(static_cast<unsigned char>(c));
+					   }),
+					   line.end());
+			if (line[0] == '#' || line.empty())
+				continue;
+			auto delimiterPos = line.find(",");
+
+			//Need To parse better To get all information
+			std::string instruction = line.substr(0, delimiterPos);
+			auto value = line.substr(delimiterPos + 1);
+
+			// Give the number of Nodes
+			if (instruction == "NODE")
+			{
+				NbNodes = std::stol(value, nullptr, 10);
+				DNodes = new Node[NbNodes + 1];
+				cout << "\n - Total nodes number : " << NbNodes;
+			}
+
+			// Give the number of Elements
+			if (instruction == "ELEM")
+			{
+				NbElems = std::stol(value, nullptr, 10);
+				cout << "\n - Total elements number : " << NbElems;
+			}
+			/* 
+// Enter in the description of Elements -> type of element & elementId, material, cross area, second moment of area / inertia, End Node A, End Node B
+			case ("EBLOCK"): 
+				break;
+
+			case "ET":
+				break;
+			case "ENDEBLOCK":
+				break;
+
+
+// Enter in the description of Coordonates of Nodes -> X, Y
+			case "NBLOCK": 
+				break;
+			case "ENDNODEBLOCK":
+				break;
+// Description of Material K,v
+			case "MAT": 
+				break;
+			case "K":
+				break;
+			case "v":
+				break;
+			case "EMAT":
+				break;
+// Description of Boundary forces
+			case "BForces": 
+				break;
+			case "ENDBForces":
+				break;
+// Description of Boundary Displacements (constraints)
+			case "BDis": 
+				break;
+			case "ENDBDis":
+				break;
+			 */
+		}
 	}
 	else
 	{
 		std::cout << "\nCan't read " << filename;
 	}
-
 	/*
-	if ((model_file = fopen(filename, "r")) != NULL)
+	NextWord(file, &Command[0]);
+	//To know how nodes is there
+	if (!strcmp("NODE", Command) && !::EndOfFile)
 	{
-		while (!::EndOfFile)
+		NextWord(file, &Command[0]);
+		sscanf(Command, "%ld", &NbNodes);
+		DNodes = new Node[NbNodes + 1];
+	}
+
+	//To know how nodes there is
+	if (!strcmp("ELEM", Command) && !::EndOfFile)
+	{
+		NextWord(file, &Command[0]);
+		sscanf(Command, "%d", &NbElems);
+	}
+
+	// Coordonates of NODE
+	if (!strcmp("NBLOCK", Command) && !::EndOfFile)
+	{
+		NextWord(file, &Command[0]);
+		inc = 0;
+		while (strcmp("ENDNODEBLOCK", Command) && !::EndOfFile)
 		{
+			DNodes[inc].Init(file);
 			NextWord(file, &Command[0]);
-			//To know how nodes is there
-			if (!strcmp("NODE", Command) && !::EndOfFile)
-			{
-				NextWord(file, &Command[0]);
-				sscanf(Command, "%ld", &NbNodes);
-				DNodes = new Node[NbNodes + 1];
-			}
-
-			//To know how nodes there is
-			if (!strcmp("ELEM", Command) && !::EndOfFile)
-			{
-				NextWord(file, &Command[0]);
-				sscanf(Command, "%d", &NbElems);
-			}
-
-			// Coordonates of NODE
-			if (!strcmp("NBLOCK", Command) && !::EndOfFile)
-			{
-				NextWord(file, &Command[0]);
-				inc = 0;
-				while (strcmp("ENDNODEBLOCK", Command) && !::EndOfFile)
-				{
-					DNodes[inc].Init(file);
-					NextWord(file, &Command[0]);
-					inc++;
-				}
-			}
-
-			//Coordonates of ELEM
-			if (!strcmp("EBLOCK", Command) && !::EndOfFile)
-			{
-				NextWord(file, &Command[0]);
-				inc = 0;
-				countlev1 = 0; // First use no element defined
-				while (strcmp("ENDEBLOCK", Command) && !::EndOfFile)
-				{
-					if (!strcmp("ET", Command))
-					{
-						NextWord(file, &Command[0]);
-						sscanf(Command, "%i", &countlev1);
-						NextWord(file, &Command[0]);
-					}
-
-					//Utilisation de CASE perhaps !...
-					// The object relating to element must be created and then deleted
-					// A simple bar : Link1 on Ansys
-					if (countlev1 == 1 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
-					{
-						DLink1[count[countlev1]] = new Link1(file, Number, DNodes); // Replace by a constructor
-					cout << "\n Reading file " << filename << " ...";
-
-	model_file.open(model_file, iosif (countlev1 == 2 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
-					{
-						DPlane2[count[countlev1]] = new Plane2(file, Number, DNodes);
-						count[countlev1]++;
-						NbObjectCreated++;
-					}
-
-					// beam3 !
-					if (countlev1 == 3 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
-					{
-						DBeam3[count[countlev1]] = new Beam3(file, Number, DNodes);
-						count[countlev1]++;
-						NbObjectCreated++;
-					}
-					if (countlev1 == 42 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
-					{
-
-						DPlane42[count[countlev1]] = new Plane42(file, Number, DNodes);
-						count[countlev1]++;
-						NbObjectCreated++;
-					}
-					NextWord(file, &Command[0]);
-				}
-			}
-cout << "\n Reading file " << filename << " ...";
-
-	model_file.open(model_file, ios
-			// Material data reading process
-			if (!strcmp("MAT", Command) && !::EndOfFile)
-			{
-				inc = 0;
-				NextWord(file, &Command[0]);
-				while (strcmp("EMAT", Command) && !::EndOfFile)
-				{
-
-					if (!strcmp("K", Command))
-					{
-						NextWord(file, &Command[0]);
-						sscanf(Command, "%le", &DMat[0].ex);
-					}
-					if (!strcmp("v", Command))
-					{
-						NextWord(file, &Command[0]);
-						sscanf(Command, "%le", &DMat[0].poisson);
-					}
-					NextWord(file, &Command[0]);
-				}
-			}
-
-			// Forces data reading process
-			if (!strcmp("BForces", Command) && !::EndOfFile)
-			{
-
-				MaxNode = 0;
-				//Calculation of the position and the maximum DOF
-				for (inc = 0; inc < NbNodes; inc++)
-				{
-					DNodes[inc].CumSum = MaxNode;
-					MaxNode += DNodes[inc].DOF;
-				}
-				Forces = new double[MaxNode];
-				for (inc = 0; inc < MaxNode; inc++)
-					Forces[inc] = 0;
-				inc = 0;
-				NextWord(file, &Command[0]);
-				while (strcmp("ENDBForces", Command) && !::EndOfFile)
-				{
-					sscanf(Command, "%ld", &Number);
-					for (inc = 0; inc < DNodes[Number - 1].DOF; inc++)
-					{
-						NextWord(file, &Command[0]);
-						sscanf(Command, "%le", &Forces[DNodes[Number - 1].CumSum + inc]);
-					}
-					NextWord(file, &Command[0]);
-				}
-			}
-
-			// Displacement data reading process
-			if (!strcmp("BDis", Command) && !::EndOfFile)
-			{
-
-				inc = 0;
-				indx = new int[MaxNode];
-				for (inc = 0; inc < MaxNode; inc++)
-					indx[inc] = 1;
-				NextWord(file, &Command[0]);
-				while (strcmp("ENDDis", Command) && !::EndOfFile)
-				{
-					sscanf(Command, "%ld", &Number);
-					for (inc = 0; inc < DNodes[Number - 1].DOF; inc++)
-					{
-						NextWord(file, &Command[0]);
-						sscanf(Command, "%i", &indx[DNodes[Number - 1].CumSum + inc]);
-					}
-					NextWord(file, &Command[0]);
-				}
-			}
+			inc++;
 		}
-		printf(" completed!\n");
-		fclose(file);
 	}
-*/
-	std::cout << "Global Matrix building...";
 
-	// Global matrix creation ! square matrix for the moment
-	double **Global = new double *[MaxNode];
-	for (inc = 0; inc < MaxNode; inc++)
-		Global[inc] = new double[MaxNode];
-
-	// Initialization of Global Matrix
-	for (inc = 0; inc < MaxNode; inc++)
-		for (int inc2 = 0; inc2 < MaxNode; inc2++)
-			Global[inc][inc2] = 0;
-
-	// Assemble Matrix
-	for (inc = 0; inc < count[1]; inc++)
+	//Coordonates of ELEM
+	if (!strcmp("EBLOCK", Command) && !::EndOfFile)
 	{
-		DLink1[inc]->CalculateLength(DNodes, DMat);
-		DLink1[inc]->AssembleMatrix(Global, MaxNode, DNodes);
-	}
-	for (inc = 0; inc < count[2]; inc++)
-	{
-		DPlane2[inc]->CalculateLength(DNodes, DMat);
-		DPlane2[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+		NextWord(file, &Command[0]);
+		inc = 0;
+		countlev1 = 0; // First use no element defined
+		while (strcmp("ENDEBLOCK", Command) && !::EndOfFile)
+		{
+			if (!strcmp("ET", Command))
+			{
+				NextWord(file, &Command[0]);
+				sscanf(Command, "%i", &countlev1);
+				NextWord(file, &Command[0]);
+			}
+
+			//Utilisation de CASE perhaps !...
+			// The object relating to element must be created and then deleted
+			// A simple bar : Link1 on Ansys
+			if (countlev1 == 1 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
+			{
+				DLink1[count[countlev1]] = new Link1(file, Number, DNodes); // Replace by a constructor
+				count[countlev1]++;
+				NbObjectCreated++;
+			}
+
+			if (countlev1 == 2 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
+			{
+				DPlane2[count[countlev1]] = new Plane2(file, Number, DNodes);
+				count[countlev1]++;
+				NbObjectCreated++;
+			}
+
+			// beam3 !
+			if (countlev1 == 3 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
+			{
+				DBeam3[count[countlev1]] = new Beam3(file, Number, DNodes);
+				count[countlev1]++;
+				NbObjectCreated++;
+			}
+			// Plane42 !
+			if (countlev1 == 42 && strcmp("ENDEBLOCK", Command) && !::EndOfFile)
+			{
+				DPlane42[count[countlev1]] = new Plane42(file, Number, DNodes);
+				count[countlev1]++;
+				NbObjectCreated++;
+			}
+			NextWord(file, &Command[0]);
+		}
 	}
 
-	for (inc = 0; inc < count[3]; inc++)
+	// Material data reading process
+	if (!strcmp("MAT", Command) && !::EndOfFile)
 	{
-		DBeam3[inc]->CalculateLength(DNodes, DMat);
-		DBeam3[inc]->AssembleMatrix(Global, MaxNode, DNodes);
-	}
-	for (inc = 0; inc < count[42]; inc++)
-	{
-		DPlane42[inc]->CalculateMatrix(DNodes, DMat);
-		DPlane42[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+		inc = 0;
+		NextWord(file, &Command[0]);
+		while (strcmp("EMAT", Command) && !::EndOfFile)
+		{
+
+			if (!strcmp("K", Command))
+			{
+				NextWord(file, &Command[0]);
+				sscanf(Command, "%le", &DMat[0].ex);
+			}
+			if (!strcmp("v", Command))
+			{
+				NextWord(file, &Command[0]);
+				sscanf(Command, "%le", &DMat[0].poisson);
+			}
+			NextWord(file, &Command[0]);
+		}
 	}
 
-	// It is a "diagonal matrix" so to implement Ks only on the diagonal so far
-	// implementation of Ks function of the Maximum degree of freedom to locate the Node
-	/* for (inc = 0; inc < MaxNode; inc++)
+	// Forces data reading process
+	if (!strcmp("BForces", Command) && !::EndOfFile)
+	{
+		MaxNode = 0;
+		//Calculation of the position and the maximum DOF
+		for (inc = 0; inc < NbNodes; inc++)
+		{
+			DNodes[inc].CumSum = MaxNode;
+			MaxNode += DNodes[inc].DOF;
+		}
+		Forces = new double[MaxNode];
+		for (inc = 0; inc < MaxNode; inc++)
+			Forces[inc] = 0;
+		inc = 0;
+		NextWord(file, &Command[0]);
+		while (strcmp("ENDBForces", Command) && !::EndOfFile)
+		{
+			sscanf(Command, "%ld", &Number);
+			for (inc = 0; inc < DNodes[Number - 1].DOF; inc++)
+			{
+				NextWord(file, &Command[0]);
+				sscanf(Command, "%le", &Forces[DNodes[Number - 1].CumSum + inc]);
+			}
+			NextWord(file, &Command[0]);
+		}
+	}
+
+	// Displacement data reading process
+	if (!strcmp("BDis", Command) && !::EndOfFile)
+	{
+		inc = 0;
+		indx = new int[MaxNode];
+		for (inc = 0; inc < MaxNode; inc++)
+			indx[inc] = 1;
+		NextWord(file, &Command[0]);
+		while (strcmp("ENDDis", Command) && !::EndOfFile)
+		{
+			sscanf(Command, "%ld", &Number);
+			for (inc = 0; inc < DNodes[Number - 1].DOF; inc++)
+			{
+				NextWord(file, &Command[0]);
+				sscanf(Command, "%i", &indx[DNodes[Number - 1].CumSum + inc]);
+			}
+			NextWord(file, &Command[0]);
+		}
+	}
+}
+printf(" completed!\n");
+fclose(file);
+}
+* /
+		std::cout
+	<< "Global Matrix building...";
+
+// Global matrix creation ! square matrix for the moment
+double **Global = new double *[MaxNode];
+for (inc = 0; inc < MaxNode; inc++)
+	Global[inc] = new double[MaxNode];
+
+// Initialization of Global Matrix
+for (inc = 0; inc < MaxNode; inc++)
+	for (int inc2 = 0; inc2 < MaxNode; inc2++)
+		Global[inc][inc2] = 0;
+
+// Assemble Matrix
+for (inc = 0; inc < count[1]; inc++)
+{
+	DLink1[inc]->CalculateLength(DNodes, DMat);
+	DLink1[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+}
+for (inc = 0; inc < count[2]; inc++)
+{
+	DPlane2[inc]->CalculateLength(DNodes, DMat);
+	DPlane2[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+}
+
+for (inc = 0; inc < count[3]; inc++)
+{
+	DBeam3[inc]->CalculateLength(DNodes, DMat);
+	DBeam3[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+}
+for (inc = 0; inc < count[42]; inc++)
+{
+	DPlane42[inc]->CalculateMatrix(DNodes, DMat);
+	DPlane42[inc]->AssembleMatrix(Global, MaxNode, DNodes);
+}
+
+// It is a "diagonal matrix" so to implement Ks only on the diagonal so far
+// implementation of Ks function of the Maximum degree of freedom to locate the Node
+/* for (inc = 0; inc < MaxNode; inc++)
 		if (indx[inc] == 0)
 			Global[inc][inc] = 1e200; */
 
